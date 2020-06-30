fbf03d5b8a109bfcd67465e177f9572dff0e155a
Suemura <39115013+Suemura@users.noreply.github.com>
2020-06-30 12:24:19 +0900

    save&undo render setting

--- apply_lut_image.py
+++ apply_lut_image.py
@@ -12,19 +12,39 @@ class LUT_OT_ExportOperator(bpy.types.Operator):
         original_path = select_image.filepath
         shutil.copyfile(original_path, temp_image)
 
+    def save_image_and_render_setting(self, context, img):
+        img_settings = {}
+        # scene settings
+        img_settings["render_file_format"] = context.scene.render.image_settings.file_format
+        img_settings["render_color_mode"] = context.scene.render.image_settings.color_mode
+        img_settings["render_color_depth"] = context.scene.render.image_settings.color_depth
+        # image settings
+        img_settings["img_colorspace_settings"] = img.colorspace_settings.name
+        img_settings["img_alpha_mode"] = img.alpha_mode
+        img_settings["img_file_format"] = img.file_format
+        img_settings["img_filepath"] = img.filepath
 
-    def save_temp_rendered_image(self, context, temp_image):
-        select_image_name = context.scene.image_list_enum
-        img = bpy.data.images[select_image_name]
-        img.colorspace_settings.name = "sRGB"
-        # render_img.use_alpha = True
+        return img_settings
 
+    def undo_image_and_render_setting(self, context, img, original_setting):
+        original_setting
+        # scene settings
+        context.scene.render.image_settings.file_format = original_setting["render_file_format"]
+        context.scene.render.image_settings.color_mode = original_setting["render_color_mode"]
+        context.scene.render.image_settings.color_depth = original_setting["render_color_depth"]
+        # image settings
+        img.colorspace_settings.name = original_setting["img_colorspace_settings"]
+        img.alpha_mode = original_setting["img_alpha_mode"]
+        img.file_format = original_setting["img_file_format"]
+        img.filepath = original_setting["img_filepath"]
+
+    def save_temp_rendered_image(self, context, temp_image, img):
         # scene settings
         context.scene.render.image_settings.file_format = "TIFF"
         context.scene.render.image_settings.color_mode = "RGB"
         context.scene.render.image_settings.color_depth = "16"
-
         # image settings
+        img.colorspace_settings.name = "sRGB"
         img.alpha_mode = "NONE"
         img.file_format = 'TIFF'
         img.filepath = temp_image
@@ -43,12 +63,18 @@ class LUT_OT_ExportOperator(bpy.types.Operator):
         temp_dir = os.path.dirname(os.path.abspath(__file__))
         temp_image = temp_dir + "\\img.tiff"
         temp_pass = temp_dir + "\\temp.tiff"
-        
-        if context.scene.image_list_enum == "Render Result":
+
+        select_image_name = context.scene.image_list_enum
+        img = bpy.data.images[select_image_name]
+        original_setting = self.save_image_and_render_setting(context, img)
+
+        if select_image_name == "Render Result":
             pass
         else:
             self.copy_current_image(context, temp_image)
-        self.save_temp_rendered_image(context, temp_image)
+        self.save_temp_rendered_image(context, temp_image, img)
         self.apply_lut(context, temp_image, temp_pass)
+
+        self.undo_image_and_render_setting(context, img, original_setting)
 
         return {"FINISHED"}
